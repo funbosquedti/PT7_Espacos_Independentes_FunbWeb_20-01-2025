d7702ae1372c6cfff1d697905495d61df108c7b5
funbosquedti <ti@funbosque.pmb.pa.gov.br>
2025-01-20 20:20:08 -0300

    PT7_Espacos_Independentes_FunbWeb_20-01-2025
    Referente aos Espaços Indepentes
    
    Desenvolvedor: André Felipe Sindeaux Sindeaux 
    Contato: (91)984478216
    
    Publicado em: 20/01/2025

---
+++ script.js
@@ -0,0 +1,25 @@
+new Vue({
+    el: '#globais',
+    data: {
+        titulo_1:'Home',
+        boasVindas:'Seja Bem-vindo(a) ao Sistema de Suporte da Funbosque!',
+        nome_dti:'DTI (Departamendo de Tecnologia da Informação)',
+        conheca:'Conheça',
+        
+        instrucoes_1:'Escolha uma das opções para solicitar um chamado, atendimento ou obter informações.',
+        instrucoes_2:'Agende seu equipamento selecionando uma das opções abaixo.',
+        instrucoes_3:'Agende e acompanhe seu pedido',
+        instrucoes_4:'Selecione um bloco para Agendamento de equipamentos multimídia.',
+        instrucoes_5:'Selecione uma sala de aula para Agendamento de equipamentos multimídia.',
+        instrucoes_6:'Selecione um Laboratório para Agendamento de equipamentos multimídia.',
+        instrucoes_7:''
+    },
+    computed:{
+
+    },
+    methods: {
+
+    }
+});
+
+
